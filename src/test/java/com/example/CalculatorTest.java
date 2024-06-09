0361993f81a0ddff06f7b55b04cbcd4f42ff1cc1
mjt <2896916839@qq.com>
2024-06-10 00:59:47 +0800

    更改

--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -9,9 +9,9 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
+        assertEquals(5, calculator.add(1, 6));
         assertEquals(-1, calculator.add(-2, 1));
-        assertEquals(0, calculator.add(0, 0));
+        assertEquals(0, calculator.add(0, 9));
     }
 
     @Test
